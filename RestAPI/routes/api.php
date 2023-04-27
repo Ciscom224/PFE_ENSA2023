8bf7a6c6b667d3e39ded61d26e1397264d66374b
Mamadou Cissé <mamadouibncisse@gmail.com>
2023-04-27 16:49:19 +0100

    mise a jour

--- RestAPI/routes/api.php
+++ RestAPI/routes/api.php
@@ -7,16 +7,7 @@ use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\Auth\UserController;
 use App\Http\Controllers\PatientController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
